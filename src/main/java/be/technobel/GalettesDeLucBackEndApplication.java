bb98948d1270d86482a731500c6464bbbe74640b
Aline Daems <Daems.aline90@gmail.com>
2024-03-04 12:26:34 +0100

    first commit

---
+++ src/main/java/be/technobel/GalettesDeLucBackEndApplication.java
@@ -0,0 +1,13 @@
+package be.technobel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalettesDeLucBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalettesDeLucBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/technobel/GalettesDeLucBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package be.technobel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalettesDeLucBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
